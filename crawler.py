6d482742b8b947c8c85bf46e5109a04bbb5a26ff
ShadiGhasemi <shadighasemi.sg@gmail.com>
2021-02-02 20:00:04 +0330

    crawler

--- crawler.py
+++ crawler.py
@@ -5,12 +5,21 @@ import re
 from selenium import webdriver
 from bs4 import BeautifulSoup
 
+from os import listdir
+import numpy as np
+
 
 class Crawler:
-    def __init__(self):
+    def __init__(self, max_cnt=5000, max_wait_thr=10):
         self.driver = self.init_driver()
         self.BASE_URL = 'https://academic.microsoft.com/paper/'
         self.PAPER_RGX = r'paper/(\d+)(/reference)?'
+        self.max_cnt = max_cnt
+        self.max_wait_thr = max_wait_thr
+        self.queue = []
+        self.init_queue()
+        self.explored = set()
+        self.extend_queue = True
 
     def init_driver(self):
         options = webdriver.ChromeOptions()
@@ -19,11 +28,43 @@ class Crawler:
                              'AppleWebKit/537.36 (KHTML, like Gecko) Chrome/88.0.4324.96 Safari/537.36"')
         return webdriver.Chrome(options=options)
 
+    def init_queue(self):
+        with open('crawler/start.txt') as file:
+            init_urls = file.readlines()
+        for url in init_urls:
+            paper_id = re.search(self.BASE_URL + r'(\d+)', url.strip())
+            if paper_id:
+                self.queue.append(paper_id.group(1))
+
+    def crawl(self):
+        cnt = 0
+        while cnt < self.max_cnt:
+            paper_id = self.queue.pop(0)
+            if paper_id in self.explored:
+                continue
+            cnt += 1
+            self.explored.add(paper_id)
+            paper = self.get_paper(paper_id)
+            if not paper['references']:
+                print(paper_id)
+            if self.extend_queue:
+                self.queue.extend(paper['references'])
+                self.extend_queue = len(set(self.queue + list(self.explored))) < self.max_cnt
+        self.driver.quit()
+
+    def wait(self):
+        cnt = 0
+        loaded = False
+        while not loaded and cnt < self.max_wait_thr:
+            time.sleep(1.5)
+            cnt += 1
+            loaded = '<div class="primary_paper">' in self.driver.page_source
+            # print(loaded)
+
     def get_html(self, paper_id):
         self.driver.get(self.BASE_URL + paper_id)
-        time.sleep(2)
-        html = self.driver.page_source
-        return BeautifulSoup(html, features='html.parser')
+        self.wait()
+        return BeautifulSoup(self.driver.page_source, features='html.parser')
 
     def make_json(self, paper_id, parsed_html):
         paper = {'id': paper_id,
@@ -37,30 +78,58 @@ class Crawler:
     def get_paper(self, paper_id):
         parsed_html = self.get_html(paper_id)
         paper = self.make_json(paper_id, parsed_html)
-        with open(f'papers/{paper_id}.json', 'w') as file:
+        with open(f'crawler/papers/{paper_id}.json', 'w') as file:
             json.dump(paper, file, indent=2)
+        return paper
 
     def get_authors(self, parsed_html):
         authors_div = parsed_html.find('div', attrs={'class': 'authors'})
         return [a.text for a in authors_div.find_all('a', attrs={'class': 'au-target author link'})]
 
     def get_references(self, parsed_html):
-        ref_lst = [a.get('href', None) for a in parsed_html.find_all('a', attrs={'class': 'title au-target'})]
-        ref_ids = []
+        ref_id_lst = []
         ref_cnt = 0
-        for ref in ref_lst:
+        primary_ref_lst = parsed_html.find_all('div', attrs={'class': 'primary_paper'})
+        for ref in primary_ref_lst:
             if ref_cnt >= 10:
                 break
-            if ref is None:
+            ref = ref.find('a', attrs={'class': 'title au-target'})
+            if ref is None or ref.get('href', None) is None:
                 continue
-            ref_id = re.search(self.PAPER_RGX, ref)
+            ref_id = re.search(self.PAPER_RGX, ref['href'])
             if ref_id:
-                ref_ids.append(ref_id.group(1))
+                ref_id_lst.append(ref_id.group(1))
                 ref_cnt += 1
 
-        return ref_ids
+        return ref_id_lst
+
+
+class PageRank:
+    def __init__(self, alpha, max_iter):
+        self.alpha = alpha
+        self.max_iter = max_iter
+        self.paper_id_lst = None
+        self.A = None
+        self.init_paper_id_lst()
+        self.init_adjacency_matrix()
+
+    def init_paper_id_lst(self):
+        file_name_lst = listdir('crawler/papers')
+        self.paper_id_lst = [file_name[:-5] for file_name in file_name_lst]
+
+    def init_adjacency_matrix(self):
+        n = len(self.paper_id_lst)
+        idx = dict(zip(self.paper_id_lst, list(range(n))))
+        self.A = np.zeros((n, n))
+        for paper_id in self.paper_id_lst:
+            with open(f'crawler/papers/{paper_id}') as file:
+                paper = json.load(file)
+            for ref in paper['references']:
+                if ref in idx:
+                    self.A[idx[paper_id], idx[ref]] = 1.
 
 
 if __name__ == '__main__':
-    crawler = Crawler()
-    crawler.get_paper('2981549002')
+    crawler = Crawler(max_cnt=50)
+    crawler.crawl()
+    # crawler.get_paper('2981549002')
